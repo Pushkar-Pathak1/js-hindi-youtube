708f7d784f23ac44a7d3ab1c0e701652f62f7a00
Pushkar-Pathak1 <pushkarpathak1572006@gmail.com>
2025-01-12 06:49:40 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //all code will be treated as newer version of javascript
+//alert("hello") we are using node js not browser
+let name="hitesh"
+let age=18
+let isLoggedIn=false
+let state;
+//number range till 2 ki power 53
+//bigint
+//string =""
+//boolean = true/false
+//null (standalone value)
+//undefined = value not assigned yet
+//symbol =uniqueness
+
+
+
+//object
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
+
+
+
