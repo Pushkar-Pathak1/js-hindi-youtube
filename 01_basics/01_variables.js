bd71bacc6a8bf5e33ca217b51734235ed6d452ee
Pushkar-Pathak1 <pushkarpathak1572006@gmail.com>
2025-01-12 06:20:39 +0000

    container confif done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "ok@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+
+
+//accountId = 2   (not allowed)
+accountEmail="k@gmail.com"
+accountPassword="098"
+accountCity="jpr"
+//dont use var because of block space
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
